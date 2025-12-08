5aa31264af500e65f776384ef58494e051fabdb1
Rajnish Sharma <srajnish714@gmail.com>
2025-12-08 21:52:12 +0530

    added pagination and created global api

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,7 +5,6 @@ import {Auth} from "../models/auth.model.js";
 const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-
 export const generateTokens = async (userId) => {
  
   const accessToken = jwt.sign({ userId: userId.toString() }, ACCESS_SECRET, { expiresIn: "15m" });
